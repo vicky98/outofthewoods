77ec015c3344b7055903c7e05fbc9be77eaeca6f
vicky98 <vikiga98@gmail.com>
2016-06-13 22:46:44 +0300

    Some updates

---
+++ app/src/main/java/outofthewoods/project/com/outofthewoods/Launcher.java
@@ -0,0 +1,14 @@
+package outofthewoods.project.com.outofthewoods;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+public class Launcher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
