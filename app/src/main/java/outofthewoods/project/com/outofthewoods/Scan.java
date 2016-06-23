a11ff1a77d307c670141b3b4a7c91ba05a3be17c
vicky98 <vikiga98@gmail.com>
2016-06-24 02:24:38 +0300

    Some changes......................

--- app/src/main/java/outofthewoods/project/com/outofthewoods/Scan.java
+++ app/src/main/java/outofthewoods/project/com/outofthewoods/Scan.java
@@ -1,7 +1,10 @@
 package outofthewoods.project.com.outofthewoods;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Scan extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class Scan extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+
+        Button button = (Button) findViewById(R.id.scan_barcode);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), BarcodeScanner.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
